3addb976f3a4c2848e2f9d3a9010f07e308c3797
Zhiznevski <w.heisenberggg@gmail.com>
2024-06-28 18:06:49 +0300

    feat: add crud operations

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -2,6 +2,7 @@ import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
+
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
@@ -11,3 +12,7 @@ export default function AcmeLogo() {
     </div>
   );
 }
+
+// const Card = ({radius})=> {
+// const className = {"br-sm": radius.small}
+// return <div className={`${className} flex flex-row `}>{children}</div>}
